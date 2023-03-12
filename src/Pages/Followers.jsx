7098aceaaf9bc76a115261ccdb0134e4ccb630a0
Palak Garg <Palakgarg296@gmail.com>
2023-03-12 17:14:58 +0530

    Improved follower page

--- src/Pages/Followers.jsx
+++ src/Pages/Followers.jsx
@@ -4,50 +4,60 @@ import { useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
 
 const Followers = () => {
-  const [fdata, setfdata] = useState([]);
+  const [followers, setFollowers] = useState([]);
   const userContext = useContext(UserContext);
   const navigate = useNavigate();
 
-  const handleClick = (user) => {
-    userContext.setUsername(user);
+  const handleClick = (username) => {
+    userContext.setUsername(username);
     navigate("/home");
   };
 
   useEffect(() => {
-    async function fetchFdata() {
-      const data = await fetch(
+    async function fetchFollowers() {
+      const response = await fetch(
         `https://api.github.com/users/${userContext.username}/followers`
       );
-      const json = await data.json();
-      setfdata(json);
+      const followersData = await response.json();
+      setFollowers(followersData);
     }
-    fetchFdata();
+    fetchFollowers();
   }, []);
 
   return (
     <>
-      <Header  />
-      <div className="h-screen bg-slate-500">
+      <Header />
+      <div className="min-h-screen bg-slate-500 flex flex-col items-center justify-center">
         <div className="bg-gray-800 w-full max-w-md rounded-md p-8">
-          {fdata.map((i) => {
-            return (
+          {followers.length > 0 ? (
+            followers.map((follower) => (
               <div
-                key={i.id}
-                className="flex items-center mb-3 cursor-pointer"
-                onClick={() => {
-                  handleClick(i.login);
-                }}
+                key={follower.id}
+                className="flex items-center mb-3 cursor-pointer hover:bg-gray-700 rounded-md p-2"
+                onClick={() => handleClick(follower.login)}
               >
                 <img
                   className="w-8 h-8 rounded-full mr-2"
-                  src={i.avatar_url}
-                  alt="avatar"
+                  src={follower.avatar_url}
+                  alt={`Avatar of ${follower.login}`}
                 />
-                <div className="text-gray-200">{i.login}</div>
+                <div className="text-gray-200 font-medium">
+                  {follower.login}
+                </div>
               </div>
-            );
-          })}
+            ))
+          ) : (
+            <div className="text-gray-200 text-center py-10">
+              Loading followers...
+            </div>
+          )}
         </div>
+        <button
+          className="absolute top-0 left-0 mt-2 ml-2 px-3 py-1 rounded-md bg-gray-800 text-white hover:bg-gray-700 focus:outline-none"
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </button>
       </div>
     </>
   );
